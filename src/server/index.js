e59731092b55ba4484fa48639705ad144c5781df
Nguyen Duy Anh <duyanh19962012@gmail.com>
2019-01-18 23:58:10 +0200

    Sync with heroku

--- src/server/index.js
+++ src/server/index.js
@@ -3,6 +3,26 @@ const os = require('os');
 
 const app = express();
 
+const { Client } = require('pg');
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true
+});
+
+client.connect();
+
+client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+  if (err) throw err;
+  //   for (const row of res.rows) {
+  //     console.log(JSON.stringify(row));
+  //   }
+  for (let row = 0; row < res.length; row + 1) {
+    console.log(JSON.stringify(res[row]));
+  }
+  client.end();
+});
+
 app.use(express.static('dist'));
 app.get('/api/getUsername', (req, res) => res.send({ username: os.userInfo().username }));
-app.listen(8080, () => console.log('Listening on port 8080!'));
+app.listen(process.env.PORT || 8080, () => console.log('Listening on port 8080!'));
