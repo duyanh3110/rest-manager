54146d61972d642e1482656ae9ed2b7d863e625b
dothuan96 <dov.thuan96@gmail.com>
2019-01-30 01:06:01 +0200

    add screen folder

--- src/client/App.js
+++ src/client/App.js
@@ -4,6 +4,7 @@ import {
   Route, Link, Redirect, BrowserRouter as Router
 } from 'react-router-dom';
 import Login from './components/auth/Login';
+import Screen from './components/screen/Screen';
 
 const Public = () => <div> This is a public page </div>;
 
@@ -18,6 +19,7 @@ export default class App extends Component {
           <Route exact path="/" component={Login} />
           <Route path="/public" component={Public} />
           <Route path="/private" component={Private} />
+          <Route path="/screen" component={Screen} />
         </div>
       </Router>
     );
