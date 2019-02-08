0c5e81f910c9f76c0b54fd94220b527908473539
Unknown <skyzerzlol@gmail.com>
2019-02-08 17:28:50 +0700

    fix dem bug on pc

--- src/server/index.js
+++ src/server/index.js
@@ -1,28 +1,73 @@
-const express = require('express');
-const os = require('os');
-
+const express = require("express");
+const os = require("os");
+const morgan = require("morgan");
+// const { Pool } = require("pg");
+const bodyParser = require("body-parser");
 const app = express();
 
-const { Client } = require('pg');
+const { Client } = require("pg");
 
 const client = new Client({
-  connectionString: process.env.DATABASE_URL,
+  // connectionString: process.env.DATABASE_URL,
+  user: "faqiaquxwsvcfl",
+  host: "ec2-54-75-245-94.eu-west-1.compute.amazonaws.com",
+  database: "d2r9omcof890g8",
+  password: "8252532b9171685b9ebf552cd307f0aaed95df071d65090b86e6b04781c0f8b1",
+  port: 5432,
+  max: 10,
+  idleTimeoutMillis: 30000,
   ssl: true
 });
 
 client.connect();
 
-client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
-  if (err) throw err;
-  //   for (const row of res.rows) {
-  //     console.log(JSON.stringify(row));
-  //   }
-  for (let row = 0; row < res.length; row + 1) {
-    console.log(JSON.stringify(res[row]));
+client.query(
+  "SELECT table_schema,table_name FROM information_schema.tables;",
+  (err, res) => {
+    if (err) throw err;
+    //   for (const row of res.rows) {
+    //     console.log(JSON.stringify(row));
+    //   }
+    for (let row = 0; row < res.length; row + 1) {
+      console.log(JSON.stringify(res[row]));
+    }
+    client.end();
   }
-  client.end();
+);
+
+app.get("/api/user", (req, res) => {
+  client.connect((err, db, done) => {
+    if (err) {
+      return console.log(err);
+    } else {
+      db.query("SELECT * FROM public.user;", (errors, user) => {
+        if (err) {
+          res.status(404).json(errors);
+        }
+        // table.rows[0].name
+        res.json(user);
+      });
+    }
+  });
+});
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(morgan("dev"));
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
 });
 
-app.use(express.static('dist'));
-app.get('/api/getUsername', (req, res) => res.send({ username: os.userInfo().username }));
-app.listen(process.env.PORT || 8080, () => console.log('Listening on port 8080!'));
+app.use(express.static("dist"));
+app.get("/api/getUsername", (req, res) =>
+  res.send({ username: os.userInfo().username })
+);
+app.listen(process.env.PORT || 8080, () =>
+  console.log("Listening on port 8080!")
+);
