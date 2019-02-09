0439799a7883afd5e86f2438ee4c1d725103b7c4
dothuan96 <dov.thuan96@gmail.com>
2019-02-09 21:22:03 +0200

    Fix Login style and reponsive

--- src/client/components/auth/Login.js
+++ src/client/components/auth/Login.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import {Bootstrap, Container, Grid, Row, Col} from 'react-bootstrap';
 import './Login.css';
 
 export default class LogIn extends Component {
@@ -21,34 +22,36 @@ export default class LogIn extends Component {
   render() {
     return (
       <div className="login-form">
-        <div className="logo">
-          <img src="public/images/logo-white.png" alt="logo" />
-        </div>
-        <form onSubmit={this.submitHandler}>
-          <div>
-            <input
-              type="email"
-              id="email"
-              onChange={this.changeHandler}
-              placeholder="Email"
-              className="input-login"
-            />
+        <div className="form">
+          <div className="logo">
+            <img src="public/images/logo-white.png" alt="logo" />
           </div>
-          <div>
-            <input
-              type="password"
-              id="password"
-              onChange={this.changeHandler}
-              placeholder="Password"
-              className="input-login"
-            />
+          <form onSubmit={this.submitHandler}>
+            <div>
+              <input
+                type="email"
+                id="email"
+                onChange={this.changeHandler}
+                placeholder="Email"
+                className="input-login"
+              />
+            </div>
+            <div>
+              <input
+                type="password"
+                id="password"
+                onChange={this.changeHandler}
+                placeholder="Password"
+                className="input-login"
+              />
+            </div>
+            <div>
+              <button className="btn-login">Sign In</button>
+            </div>
+          </form>
+          <div className="sign-up">
+            <a href="#">Create a new account</a>
           </div>
-          <div>
-            <button className="btn-login">Sign In</button>
-          </div>
-        </form>
-        <div className="sign-up">
-          <a href="#">Create a new account</a>
         </div>
       </div>
     );
