40914fae3284436ced2cb861d7814574f3a83d08
Unknown <skyzerzlol@gmail.com>
2019-02-14 10:22:08 +0200

    check if database working

--- src/server/index.js
+++ src/server/index.js
@@ -4,6 +4,9 @@ const morgan = require("morgan");
 // const { Pool } = require("pg");
 const bodyParser = require("body-parser");
 const app = express();
+const path = require("path");
+
+const users = require("./router/api/users");
 
 const { Client } = require("pg");
 
@@ -19,37 +22,37 @@ const client = new Client({
   ssl: true
 });
 
-client.connect();
+// client.connect();
 
-client.query(
-  "SELECT table_schema,table_name FROM information_schema.tables;",
-  (err, res) => {
-    if (err) throw err;
-    //   for (const row of res.rows) {
-    //     console.log(JSON.stringify(row));
-    //   }
-    for (let row = 0; row < res.length; row + 1) {
-      console.log(JSON.stringify(res[row]));
-    }
-    client.end();
-  }
-);
+// client.query(
+//   "SELECT table_schema,table_name FROM information_schema.tables;",
+//   (err, res) => {
+//     if (err) throw err;
+//     //   for (const row of res.rows) {
+//     //     console.log(JSON.stringify(row));
+//     //   }
+//     for (let row = 0; row < res.length; row + 1) {
+//       console.log(JSON.stringify(res[row]));
+//     }
+//     client.end();
+//   }
+// );
 
-app.get("/api/user", (req, res) => {
-  client.connect((err, db, done) => {
-    if (err) {
-      return console.log(err);
-    } else {
-      db.query("SELECT * FROM public.user;", (errors, user) => {
-        if (err) {
-          res.status(404).json(errors);
-        }
-        // table.rows[0].name
-        res.json(user);
-      });
-    }
-  });
-});
+// app.get("/api/user", (req, res) => {
+//   client.connect((err, db, done) => {
+//     if (err) {
+//       return console.log(err);
+//     } else {
+//       db.query("SELECT * FROM public.user;", (errors, user) => {
+//         if (err) {
+//           res.status(404).json(errors);
+//         }
+//         // table.rows[0].name
+//         res.json(user);
+//       });
+//     }
+//   });
+// });
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -68,6 +71,8 @@ app.use(express.static("dist"));
 app.get("/api/getUsername", (req, res) =>
   res.send({ username: os.userInfo().username })
 );
+
+app.use("/api/users", users);
 app.listen(process.env.PORT || 8080, () =>
   console.log("Listening on port 8080!")
 );
---
+++ src/server/router/api/users.js
@@ -0,0 +1,67 @@
+const express = require("express");
+const router = express.Router();
+
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const { Client } = require("pg");
+const passport = require("passport");
+
+const validateLoginInput = require("../../validation/login");
+const validateRegisterInput = require("../../validation/register");
+
+const client = new Client({
+  // connectionString: process.env.DATABASE_URL,
+  user: "faqiaquxwsvcfl",
+  host: "ec2-54-75-245-94.eu-west-1.compute.amazonaws.com",
+  database: "d2r9omcof890g8",
+  password: "8252532b9171685b9ebf552cd307f0aaed95df071d65090b86e6b04781c0f8b1",
+  port: 5432,
+  max: 10,
+  idleTimeoutMillis: 30000,
+  ssl: true
+});
+
+router.post("/test", (req, res) => res.json({ msg: "Users work" }));
+
+//Login
+router.post("/login", (req, res) => {
+  const { errors, isValid } = validateLoginInput(req.body);
+
+  // Check Validation
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
+  res.json({ msg: "Users work" });
+});
+
+//Register
+router.post("/register", (req, res) => {
+  const { errors, isValid } = validateRegisterInput(req.body);
+
+  // Check Validation
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
+  res.json({ msg: "Users work" });
+});
+
+//list all users
+router.get("/allusers", (req, res) => {
+  client.connect((err, db, done) => {
+    if (err) {
+      return console.log(err);
+    } else {
+      db.query("SELECT * FROM public.user;", (errors, user) => {
+        if (err) {
+          res.status(404).json(errors);
+        }
+        // table.rows[0].name
+        res.json(user);
+      });
+    }
+  });
+});
+
+module.exports = router;
